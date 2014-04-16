90e8f8289268a3f3b87e8aec15c5209be2b9b4e1
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-04-16 16:16:57 +0000

    Removed large gun file that doesn't work 100%
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@271 c70e0d21-08ce-ef44-a38d-91c21494a941

---
+++ Engine/Engine/LevelFileHeader.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "uint.h"
+
+struct header {
+	uint numOfNodes;
+	uint numOfConnections;
+	uint startOfBinaryData;
+	uint startOfNodeData;
+	uint startOfonnectionData;
+	inline 
+};
