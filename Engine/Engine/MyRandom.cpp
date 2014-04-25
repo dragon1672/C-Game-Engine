0c973694fcec0dd4d22fae60c178b6db77287baa
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-04-25 03:56:29 +0000

    expanding random for GLM
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@320 c70e0d21-08ce-ef44-a38d-91c21494a941

--- Engine/Engine/MyRandom.cpp
+++ Engine/Engine/MyRandom.cpp
@@ -40,3 +40,18 @@ Vector2D Random::randomFloatVector(float low, float high) {
 Vector2D Random::randomIntVector(int   low, int   high) {
 	return Vector2D((float)randomInt(low,high),(float)randomInt(low,high));
 }
+glm::vec3 Random::glmRand::randomUnitVector() {
+	return glm::normalize(glm::vec3(randomFloat(-1,1),randomFloat(-1,1),randomFloat(-1,1)));
+}
+glm::vec3 Random::glmRand::randomFloatVectorInBox(float width, float height, float depth) {
+	return glm::vec3(randomFloat(0,width),randomFloat(0,height),randomFloat(0,depth));
+}
+glm::vec3 Random::glmRand::randomIntVectorInBox(int   width, int   height, int depth) {
+	return glm::vec3((float)randomInt(0,width),(float)randomInt(0,height),randomInt(0,depth));
+}
+glm::vec3 Random::glmRand::randomFloatVector(float low, float high) {
+	return glm::vec3(randomFloat(low,high), randomFloat(low,high), randomFloat(low, high));
+}
+glm::vec3 Random::glmRand::randomIntVector(int   low, int   high) {
+	return glm::vec3((float)randomInt(low,high), (float)randomInt(low,high), (float)randomInt(low,high));
+}
--- Engine/Engine/MyRandom.h
+++ Engine/Engine/MyRandom.h
@@ -3,6 +3,7 @@
 #define MY_RANDOM_H
 
 #include "ExportHeader.h"
+#include <glm\glm.hpp>
 
 class Vector2D;
 
@@ -17,7 +18,14 @@ namespace Random {
 	ENGINE_SHARED Vector2D randomIntVectorInBox  (int   width, int   height);
 	ENGINE_SHARED Vector2D randomFloatVector(float low, float high);
 	ENGINE_SHARED Vector2D randomIntVector  (int   low, int   high);
-	ENGINE_SHARED void     init();
+	namespace glmRand {
+		ENGINE_SHARED glm::vec3 randomUnitVector();
+		ENGINE_SHARED glm::vec3 randomFloatVectorInBox(float width, float height, float depth = 0);
+		ENGINE_SHARED glm::vec3 randomIntVectorInBox  (int   width, int   height, int   depth = 0);
+		ENGINE_SHARED glm::vec3 randomFloatVector(float low, float high);
+		ENGINE_SHARED glm::vec3 randomIntVector  (int   low, int   high);
+	}
+	ENGINE_SHARED void     init(); // not required
 };
 
 #endif//MY_RANDOM_H
