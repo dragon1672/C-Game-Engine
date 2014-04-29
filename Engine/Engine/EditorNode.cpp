3879fe63839d51d87967a2fe646a9d85c457b2c4
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-04-29 03:40:17 +0000

    Organization and readme updated
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@341 c70e0d21-08ce-ef44-a38d-91c21494a941

--- Engine/Engine/EditorNode.cpp
+++ Engine/Engine/EditorNode.cpp
@@ -72,14 +72,7 @@ void EditorNode::setConnectionState(bool state) {
 	}
 }
 bool EditorNode::doesRayHit(Ray& ray) {
-	glm::vec3 raytoCircle_C = pos - ray.origin;
-	glm::vec3 rayExtended_A = glm::proj(raytoCircle_C,ray.direction);
-
-	float raytoCircleLength = glm::dot(raytoCircle_C,raytoCircle_C); // returns length squared
-	float rayExtendedLength = glm::dot(rayExtended_A,rayExtended_A); // returns length squared
-	float collisionLength = raytoCircleLength - rayExtendedLength;
-
-	return collisionLength < radius * radius;
+	return doesRayHit(ray,this->pos,1);
 }
 bool EditorNode::doesRayHit(Ray& ray, glm::vec3 orgin, float radius) {
 	glm::vec3 raytoCircle_C = orgin - ray.origin;
