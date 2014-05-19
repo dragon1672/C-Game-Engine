91ba00fdf197a3c76d02dcdf96bf36c24cec0a8b
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-05-19 19:13:04 +0000

    better balance, improved memory
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@405 c70e0d21-08ce-ef44-a38d-91c21494a941

--- Engine/Engine/SpringForceGenerator.h
+++ Engine/Engine/SpringForceGenerator.h
@@ -32,8 +32,7 @@ public:
 	void removeSpring(Particle * leParticle, glm::vec3 * anchor) {
 		auto id = springs.find(leParticle);
 		if (id != springs.end()) {
-			std::vector<glm::vec3*> data = springs[leParticle];
-			springs.erase(leParticle);
+			std::vector<glm::vec3*>& data = springs[leParticle];
 			for (int i = 0; i < data.size(); i++)
 			{
 				if(data[i] == anchor) {
@@ -41,7 +40,10 @@ public:
 					i--;
 				}
 			}
-			springs.emplace(leParticle,data);
+			//wonder if this is worth it
+			if(data.size() <= 0) {
+				springs.erase(leParticle);
+			}
 		}
 	}
 	
@@ -49,7 +51,7 @@ public:
 	void updateForce(Particle * toUpdate) {
 		auto id = springs.find(toUpdate);
 		if (id != springs.end()) {
-			std::vector<glm::vec3 *> anchors = springs[toUpdate];
+			std::vector<glm::vec3 *>& anchors = springs[toUpdate];
 			for (int i = 0; i < anchors.size(); i++)
 			{
 				glm::vec3 anchor = *anchors[i];
