fbb36838fbc991adedf1dd56db21d25944d52a07
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-05-22 03:25:57 +0000

    Collision, drag and drop particles
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@412 c70e0d21-08ce-ef44-a38d-91c21494a941

---
+++ Engine/Engine/CollisionMassCircles.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "CollisionEngine.h"
+#include "Particle.h"
+#include <glm/gtx/projection.hpp>
+#include "ExportHeader.h"
+
+class ENGINE_SHARED CollisionMassCircles : public CollisionEngine {
+public:
+	Particle* particles[2];
+	glm::vec3 vels[2];
+	glm::vec3 collisionNorm;
+	float pen;
+
+	void init(Particle * p1, Particle* p2) {
+		particles[0] = p1;
+		particles[1] = p2;
+	}
+	void update() {
+		glm::vec3 diff = particles[0]->pos - particles[1]->pos;
+		float diffLength = glm::length(diff);
+		collisionNorm = diff / diffLength;
+		pen = (particles[0]->mass/2 + particles[1]->mass/2) - diffLength;
+		vels[0] = glm::proj(particles[0]->vel,collisionNorm);
+		vels[1] = glm::proj(particles[1]->vel,collisionNorm);
+	}
+	bool hasCollided() {
+		return circleCollide(particles[0]->pos,particles[0]->mass/2,particles[1]->pos,particles[1]->mass/2)
+			&& glm::dot(vels[0] - vels[1],collisionNorm);
+	}
+	float getPenetration() {
+		return pen;
+	}
+	glm::vec3 getNormal() {
+		return collisionNorm;
+	}
+	bool circleCollide(glm::vec3& object1Pos, float object1Rad, glm::vec3& object2Pos, float object2Rad) {
+		glm::vec3 diff = object1Pos - object2Pos;
+		float rads = object1Rad + object2Rad;
+		return glm::dot(diff,diff) < rads * rads;
+	}
+};
--- Engine/Engine/ParticleContact.h
+++ Engine/Engine/ParticleContact.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Particle.h"
+#include <glm\gtx\projection.hpp>
 
 class ParticleContact
 {
@@ -9,24 +10,18 @@ public:
 	float restitution;
 	glm::vec3 contactNormal;
 	float penetration;
-	void resolve(float dt) {
+
+	void collide(float dt) {
 		resolveVelocity(dt);
 		resolveInterpenetration(dt);
 	}
 	void resolveInterpenetration(float dt) {
 		glm::vec3 particleMovement[2];
-		// If we don’t have any penetration, skip this step.
 		if (penetration <= 0) return;
-		// The movement of each object is based on their inverse mass,
-		// so total that.
 		float totalInverseMass = 1/particle[0]->mass;
 		if (particle[1]) totalInverseMass += (1/particle[1]->mass);
-		// If all particles have infinite mass, then we do nothing.
 		if (totalInverseMass <= 0) return;
-		// Find the amount of penetration resolution per unit
-		// of inverse mass.
 		glm::vec3 movePerIMass = contactNormal * (penetration / totalInverseMass);
-		// Calculate the movement amounts.
 		particleMovement[0] = movePerIMass * (1/particle[0]->mass);
 		if (particle[1] != nullptr) {
 			particleMovement[1] = movePerIMass * -particle[1]->mass;
@@ -34,48 +29,27 @@ public:
 		} else {
 			particleMovement[1] = glm::vec3();
 		}
-		// Apply the penetration resolution.
 		particle[0]->pos += particleMovement[0];
 	}
-
+	glm::vec3 getVel(int index) const {
+		return particle[index] != nullptr ? glm::proj(particle[index]->vel,contactNormal) : glm::vec3();
+	}
 	float calculateSeparatingVelocity() const {
-		glm::vec3 relativeVelocity = particle[0]->vel;
-
-		if (particle[1]) relativeVelocity -= particle[1]->vel;
-
+		glm::vec3 relativeVelocity = getVel(0) - getVel(1);
 		float ret = glm::dot(relativeVelocity, contactNormal);
 		return ret;
 	}
-private:
 	void resolveVelocity(float dt) {
-		// Find the velocity in the direction of the contact.
 		float separatingVelocity = calculateSeparatingVelocity();
-		// Check if it needs to be resolved.
-		if (separatingVelocity > 0) {
-			// The contact is either separating, or stationary;
-			// no impulse is required.
-			return;
-		}
-		// Calculate the new separating velocity.
 		float newSepVelocity = -separatingVelocity * restitution;
 		float deltaVelocity = newSepVelocity - separatingVelocity;
-		// We apply the change in velocity to each object in proportion to
-		// their inverse mass (i.e., those with lower inverse mass [higher
-		// actual mass] get less change in velocity).
 		float totalInverseMass = (1/particle[0]->mass);
 		if (particle[1]!= nullptr) totalInverseMass += (1/particle[1]->mass);
-		// If all particles have infinite mass, then impulses have no effect.
 		if (totalInverseMass <= 0) return;
-		// Calculate the impulse to apply.
 		float impulse = deltaVelocity / totalInverseMass;
-		// Find the amount of impulse per unit of inverse mass.
 		glm::vec3 impulsePerIMass = contactNormal * impulse;
-		// Apply impulses: they are applied in the direction of the contact,
-		// and are proportional to the inverse mass.
 		particle[0]->vel += impulsePerIMass * (1/particle[0]->mass);
-
 		if (particle[1] != nullptr) {
-			// Particle 1 goes in the opposite direction
 			particle[1]->vel += impulsePerIMass * -(1/particle[1]->mass);
 		}
 	}
