c54d69d6aa006fb4ade2f6adc1f0a869f57c5731
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-05-25 04:56:58 +0000

    Adding bounce lab
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@419 c70e0d21-08ce-ef44-a38d-91c21494a941

---
+++ Engine/Engine/CollisionMassWall.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CollisionEngine.h"
+#include "Particle.h"
+#include <glm/gtx/projection.hpp>
+#include "ExportHeader.h"
+#include "Ray.h"
+
+class ENGINE_SHARED CollisionMassWall: public CollisionEngine {
+public:
+	Particle* particle;
+	Ray * wall;
+
+	glm::vec3 vel;
+	float pen;
+
+	void init(Particle * p1, Ray * wall) {
+		particle = p1;
+		this->wall = wall;
+	}
+	void update() {
+		glm::vec3 diff = particle->pos - wall->origin;
+		glm::vec3 correctDiff = glm::proj(diff,wall->direction);
+		float rad = (particle->mass/2);
+		pen = rad - glm::length(correctDiff);
+		vel = glm::proj(particle->vel,wall->direction);
+	}
+	bool hasCollided() {
+		return pen >= 0
+			&& glm::dot(vel,wall->direction) <=0;
+	}
+	float getPenetration() {
+		return pen;
+	}
+	glm::vec3 getNormal() {
+		return wall->direction;
+	}
+}; 
