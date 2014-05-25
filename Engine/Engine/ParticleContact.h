5062f1df14f0d2294d20eac819ea574419dfb2bb
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-05-25 04:56:29 +0000

    Accounting for resting collision
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@418 c70e0d21-08ce-ef44-a38d-91c21494a941

--- Engine/Engine/ParticleContact.h
+++ Engine/Engine/ParticleContact.h
@@ -3,6 +3,8 @@
 #include "Particle.h"
 #include <glm\gtx\projection.hpp>
 
+#define MAX(a,b) ( a < b ? b : a)
+
 class ParticleContact
 {
 public:
@@ -16,8 +18,8 @@ public:
 		resolveInterpenetration(dt);
 	}
 	void resolveInterpenetration(float dt) {
-		glm::vec3 particleMovement[2];
 		if (penetration <= 0) return;
+		glm::vec3 particleMovement[2];
 		float totalInverseMass = 1/particle[0]->mass;
 		if (particle[1]) totalInverseMass += (1/particle[1]->mass);
 		if (totalInverseMass <= 0) return;
@@ -34,14 +36,28 @@ public:
 	glm::vec3 getVel(int index) const {
 		return particle[index] != nullptr ? glm::proj(particle[index]->vel,contactNormal) : glm::vec3();
 	}
+	glm::vec3 getForce(int index) const {
+		return particle[index] != nullptr ? glm::proj(particle[index]->totalForce,contactNormal) : glm::vec3();
+	}
 	float calculateSeparatingVelocity() const {
 		glm::vec3 relativeVelocity = getVel(0) - getVel(1);
 		float ret = glm::dot(relativeVelocity, contactNormal);
 		return ret;
 	}
 	void resolveVelocity(float dt) {
+		if(particle[1] != nullptr) particle[1]->totalForce -= getForce(1);
 		float separatingVelocity = calculateSeparatingVelocity();
 		float newSepVelocity = -separatingVelocity * restitution;
+		
+		//for resting collision
+		glm::vec3 accCollision = particle[0]->getAcc() - ((particle[1]!=nullptr) ? particle[1]->getAcc() : glm::vec3());
+		float projectedAcc = glm::dot(accCollision,contactNormal) * dt;
+		if(projectedAcc < 0) {
+			newSepVelocity += restitution * projectedAcc;
+			newSepVelocity = MAX(newSepVelocity,0);
+		}
+		particle[0]->totalForce -= getForce(0);
+
 		float deltaVelocity = newSepVelocity - separatingVelocity;
 		float totalInverseMass = (1/particle[0]->mass);
 		if (particle[1]!= nullptr) totalInverseMass += (1/particle[1]->mass);
