815b49e900633fe3bad34728bdd418e21e46bb44
acorbin <acorbin@cdf02570-8df7-3f47-b8d9-5cc645ce1d61>
2014-06-25 20:39:12 +0000

    Adding MiddleWare
    git-svn-id: https://svn.neumont.edu:8443/svn/engine_renderer_src@1 cdf02570-8df7-3f47-b8d9-5cc645ce1d61

--- Engine/Engine/ParticleForceGenerator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Particle.h"
-
-class ParticleForceGenerator {
-public:
-	virtual void updateForce(Particle * ctxt) = 0;
-};
---
+++ MiddleWare/NeumontTools/include/Slider.h
@@ -0,0 +1,28 @@
+#ifndef NEUMONT_SLIDER_H
+#define NEUMONT_SLIDER_H
+#include <QtGui\qwidget>
+class QSlider;
+class QLabel;
+
+namespace Neumont
+{
+	class Slider : public QWidget
+	{
+		Q_OBJECT
+
+		QSlider* slider;
+		QLabel* label;
+
+	private slots:
+		void sliderValueChanged();
+
+	signals:
+		void valueChanged(float newValue);
+
+	public:
+		Slider(float min = -10.0f, float max = 10.0f, bool textOnLeft = false);
+		float value() const;
+		void setValue(float newValue);
+	};
+}
+#endif
