2603f7294d45c60302f31ecc00390934ab1706d4
acorbin <acorbin@cdf02570-8df7-3f47-b8d9-5cc645ce1d61>
2014-06-28 07:04:18 +0000

    Re Moc (bat files works with directors WOOT)
    git-svn-id: https://svn.neumont.edu:8443/svn/engine_renderer_src@14 cdf02570-8df7-3f47-b8d9-5cc645ce1d61

--- Engine/Engine/Engine/Tools/QT/ButtonInfo_mock.cpp
+++ Engine/Engine/Engine/Tools/QT/ButtonInfo_mock.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include <Engine\Tools\QT\ButtonInfo.h>
+#include "ButtonInfo.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'ButtonInfo.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 63
