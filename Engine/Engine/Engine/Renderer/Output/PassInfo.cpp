8b6c4ec11f71e2e1fb63b7f4c2f4d5eceda7d0d7
Anthony Corbin <dragon1672@gmail.com>
2014-07-21 23:25:31 -0600

    Showing error code

--- Engine/Engine/Engine/Renderer/Output/PassInfo.cpp
+++ Engine/Engine/Engine/Renderer/Output/PassInfo.cpp
@@ -1,6 +1,7 @@
 #include <GL\glew.h>
 #include "PassInfo.h"
 #include <Engine\Renderer\Shader\ShaderProgram.h>
+#include <Qt/qdebug.h>
 
 void PassInfo::initTextures(int width, int height) {
 	glGenFramebuffers(1,&frameBufferID);
@@ -9,6 +10,12 @@ void PassInfo::initTextures(int width, int height) {
 	uint getDepthTexture = ShaderProgram::load2DTexture(0,width,height,GL_DEPTH_COMPONENT32);
 	glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER,GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, getColorTexture,0);
 	glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER,GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, getDepthTexture,0);
+	uint status = glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER);
+	if(status != GL_FRAMEBUFFER_COMPLETE) { // returning error code: 36054, incomplete frame
+		static int power = 0;
+
+		qDebug() << "THE BUFFER (" << status << ") - ----- - - - - - - - - - - - - - - - --- -- - - - -  - -- -- -";
+	}
 }
 void PassInfo::activate() {
 	glBindFramebuffer(GL_FRAMEBUFFER, frameBufferID);
