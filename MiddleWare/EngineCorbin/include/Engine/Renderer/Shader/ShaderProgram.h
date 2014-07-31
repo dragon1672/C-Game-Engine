71730c5f9c586cc78098dbd00d93ee3299b1bb05
Anthony Corbin <dragon1672@gmail.com>
2014-07-30 20:42:29 -0600

    Exported engine

--- MiddleWare/EngineCorbin/include/Engine/Renderer/Shader/ShaderProgram.h
+++ MiddleWare/EngineCorbin/include/Engine/Renderer/Shader/ShaderProgram.h
@@ -11,6 +11,7 @@
 #include "ExportHeader.h"
 #include <Engine\Renderer\Shader\ShaderUniformPram.h>
 #include <Engine\unsigned.h>
+#include <glm\glm.hpp>
 
 class ENGINE_SHARED ShaderProgram {
 private:
@@ -52,6 +53,11 @@ public:
 	void passUniform(uint location, ParameterType parameterType, const void * value);
 
 	//use these to add shader wide uniforms
+	void saveUniform(const char * name, const float& value);
+	void saveUniform(const char * name, const glm::vec3& value);
+	void saveUniform(const char * name, const glm::vec4& value);
+	void saveUniform(const char * name, const glm::mat3& value);
+	void saveUniform(const char * name, const glm::mat4& value);
 	void saveUniform(const char* name, ParameterType parameterType, const void * value);
 
 	//used in draw calls
@@ -60,16 +66,48 @@ public:
 	bool getValidPush();
 	void resetValidPush();
 
-	static QImage getImageFromFile(QString filePath, bool flipHorz = false, bool flipVert = false);
+	static QImage getImageFromFile(QString& filePath, bool flipHorz = false, bool flipVert = false);
 
-	//returns the bufferID
-	static GLuint load2DTexture(QImage image, GLenum type = GL_RGBA);
-	static GLuint load2DTexture(QString filePath, bool flipHorz = false, bool flipVert = false);
+	struct ImageData {
+		ubyte * data;
+		uint width;
+		uint height;
+		GLenum type;
+		GLenum type2;
+		ImageData() : data(nullptr),
+					width(-1), height(-1),
+					type(GL_RGBA), type2(-1) {}
+		ImageData(QImage& src) {
+			init(src);
+			type = GL_RGBA;
+			type2 = -1;
+		}
+		void init(QImage& src) {
+			data = src.bits();
+			width = src.width();
+			height = src.height();
+		}
+	};
+
+	//returns the slotID
+	static GLuint load2DTexture(QImage& image, GLenum type = GL_RGBA);
+	static GLuint load2DTexture(QImage& image, GLenum type, GLenum type2);
+	static GLuint load2DTexture(QString& filePath, bool flipHorz = false, bool flipVert = false);
 	static GLuint load2DTexture(ubyte * data, uint width, uint height, GLenum type = GL_RGBA);
+	static GLuint load2DTexture(ubyte * data, uint width, uint height, GLenum type, GLenum type2);
+	static GLuint load2DTexture(ImageData& imageData);
 
-	static void update2DTexture(uint texture, QImage image, GLenum type = GL_RGBA);
-	static void update2DTexture(uint texture, QString filePath, bool flipHorz = false, bool flipVert = false);
+	static void update2DTexture(uint texture, QImage& image, GLenum type = GL_RGBA);
+	static void update2DTexture(uint texture, QImage& image, GLenum type, GLenum type2);
+	static void update2DTexture(uint texture, QString& filePath, bool flipHorz = false, bool flipVert = false);
 	static void update2DTexture(uint texture, ubyte * data, uint width, uint height, GLenum type = GL_RGBA);
+	static void update2DTexture(uint texture, ubyte * data, uint width, uint height, GLenum type, GLenum type2);
+	static void update2DTexture(uint texture, ImageData& imageData);
+
+	static GLuint loadCubeTexture(QString& posX,QString& negX,QString& posY,QString& negY,QString& posZ,QString& negZ);
+	static GLuint loadCubeTexture(QString& directory,QString& posX,QString& negX,QString& posY,QString& negY,QString& posZ,QString& negZ);
+	static GLuint loadCubeTexture(ImageData& posX,ImageData& negX,ImageData& posY,ImageData& negY,ImageData& posZ,ImageData& negZ);
+	static GLuint loadCubeTexture(QImage& posX,QImage& negX,QImage& posY,QImage& negY,QImage& posZ,QImage& negZ);
 };
 
 #endif
