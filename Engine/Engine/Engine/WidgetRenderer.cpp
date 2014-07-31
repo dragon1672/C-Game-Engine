f50d74ebf5d2bfacb883842cbe3422fa042725d3
Anthony Corbin <dragon1672@gmail.com>
2014-07-31 14:45:28 -0600

    Expanding default shaders, cam moves with mouse wheel

--- Engine/Engine/Engine/WidgetRenderer.cpp
+++ Engine/Engine/Engine/WidgetRenderer.cpp
@@ -5,47 +5,69 @@
 #include <QtGui\qkeyevent>
 
 #pragma region shaderCode
-const char * defaultVertShader = "#version 400                      \n"
-	"																\n"
-	"in layout(location=0) vec3 pos;								\n"
-	"in layout(location=1) vec3 col;								\n"
-	"in layout(location=3) vec2 uv;									\n"
-	"																\n"
-	"out vec2 outUv;												\n"
-	"out vec3 outCol;												\n"
-	"																\n"
-	"//mats															\n"
-	"uniform mat4x4 viewTransform;									\n"
-	"uniform mat4x4 model2WorldTransform;							\n"
-	"																\n"
-	"void main() {													\n"
-	"	vec4 transformedPos =  model2WorldTransform * vec4(pos,1);  \n"
-	"	gl_Position =  viewTransform * transformedPos;				\n"
-	"	outUv = uv;													\n"
-	"	outCol = col;												\n"
-	"}"
-	"";
+const char * defaultVertShader = "#version 400					  \n"
+	"															  \n"
+	"in layout(location=0) vec3 pos;							  \n"
+	"in layout(location=1) vec3 col;							  \n"
+	"in layout(location=3) vec2 uv;								  \n"
+	"															  \n"
+	"out vec2 fragUv;											  \n"
+	"out vec3 fragCol;											  \n"
+	"out vec3 fragPos;											  \n"
+	"out vec3 fragWorldPos;										  \n"
+	"															  \n"
+	"//mats														  \n"
+	"uniform mat4x4 viewTransform;								  \n"
+	"uniform mat4x4 model2WorldTransform;						  \n"
+	"															  \n"
+	"void main() {												  \n"
+	"	vec4 transformedPos = model2WorldTransform * vec4(pos,1); \n"
+	"	gl_Position = viewTransform * transformedPos;			  \n"
+	"	fragUv = uv;											  \n"
+	"	fragCol = col;											  \n"
+	"	fragPos = pos;											  \n"
+	"	fragWorldPos = vec3(transformedPos);					  \n"
+	"}															  \n"
+	"															  \n";
 //color
-const char * defaultFragShader_Color = "#version 400       \n"
-	"													   \n"
-	"in vec3 outCol;									   \n"
-	"													   \n"
-	"void main() {										   \n"
-	"	gl_FragColor = vec4(outCol,1);                     \n"
-	"}													   \n"
+const char * defaultFragShader_Color = "#version 400\n"
+	"												\n"
+	"in vec3 fragCol;								\n"
+	"												\n"
+	"void main() {									\n"
+	"	gl_FragColor = vec4(fragCol,1);				\n"
+	"}												\n"
 	"";
 //texture
-const char * defaultFragShader_Texture = "#version 400     \n"
+const char * defaultFragShader_Texture = "#version 400\n"
+	"												  \n"
+	"in vec2 fragUv;								  \n"
+	"uniform sampler2D myTexture;					  \n"
+	"												  \n"
+	"void main() {									  \n"
+	"	gl_FragColor = texture(myTexture,fragUv);	  \n"
+	"}												  \n"
+	"";
+//Cube map withModeldPos
+const char * defaultFragShader_CubeMapModel= "#version 400\n"
+	"													  \n"
+	"in vec3 fragPos;									  \n"
+	"uniform samplerCube worldMap;						  \n"
+	"													  \n"
+	"void main() {										  \n"
+	"	gl_FragColor = texture(worldMap,fragPos);		  \n"
+	"}													  \n"
+	"";
+//Cube map withWorldPos
+const char * defaultFragShader_CubeMapWorld = "#version 400\n"
 	"													   \n"
-	"in vec2 outUv;										   \n"
-	"uniform sampler2D myTexture;                          \n"
+	"in vec3 fragWorldPos;								   \n"
+	"uniform samplerCube worldMap;						   \n"
 	"													   \n"
 	"void main() {										   \n"
-	"	gl_FragColor = vec4(1,1,1,1);                      \n"
-	"	gl_FragColor = texture(myTexture,outUv);           \n"
-	"	gl_FragColor.a = 1;                                \n"
+	"	gl_FragColor = texture(worldMap,fragWorldPos);	   \n"
 	"}													   \n"
-	"";
+	"													   \n";
 #pragma endregion
 #pragma region HUD assets
 
@@ -80,18 +102,34 @@ void WidgetRenderer::initializeGL() {
 	setMouseTracking(true);
 
 	//setting up default shader
+	//ShaderProgram * passThroughColor;    // see code set A
 	bool noDefaultShaderErrors = mainShader->addProgram_srcCode(defaultVertShader,      GL_VERTEX_SHADER);		assert(noDefaultShaderErrors);
 		 noDefaultShaderErrors = mainShader->addProgram_srcCode(defaultFragShader_Color,GL_FRAGMENT_SHADER);	assert(noDefaultShaderErrors);
 	mainShader->linkAndRun();
 	mainShader->saveUniform("viewTransform",ParameterType::PT_MAT4,&viewTransform[0][0]);
 
 	defaultShaders.passThroughColor = mainShader;
+	//ShaderProgram * passThroughTexture;  // see code set B
 	defaultShaders.passThroughTexture = addShader();
-
 	noDefaultShaderErrors = defaultShaders.passThroughTexture->addProgram_srcCode(defaultVertShader,        GL_VERTEX_SHADER);		assert(noDefaultShaderErrors);
 	noDefaultShaderErrors = defaultShaders.passThroughTexture->addProgram_srcCode(defaultFragShader_Texture,GL_FRAGMENT_SHADER);	assert(noDefaultShaderErrors);
 	defaultShaders.passThroughTexture->linkAndRun();
 	defaultShaders.passThroughTexture->saveUniform("viewTransform",ParameterType::PT_MAT4,&viewTransform[0][0]);
+
+
+	//ShaderProgram * passThroughCubeMap;  // see code set C
+	defaultShaders.passThroughCubeMap = addShader();
+	noDefaultShaderErrors = defaultShaders.passThroughCubeMap->addProgram_srcCode(defaultVertShader,             GL_VERTEX_SHADER);		assert(noDefaultShaderErrors);
+	noDefaultShaderErrors = defaultShaders.passThroughCubeMap->addProgram_srcCode(defaultFragShader_CubeMapModel,GL_FRAGMENT_SHADER);	assert(noDefaultShaderErrors);
+	defaultShaders.passThroughCubeMap->linkAndRun();
+	defaultShaders.passThroughCubeMap->saveUniform("viewTransform",ParameterType::PT_MAT4,&viewTransform[0][0]);
+
+	//ShaderProgram * passThroughWCubeMap; // see code set D
+	defaultShaders.passThroughWCubeMap = addShader();
+	noDefaultShaderErrors = defaultShaders.passThroughWCubeMap->addProgram_srcCode(defaultVertShader,             GL_VERTEX_SHADER);	assert(noDefaultShaderErrors);
+	noDefaultShaderErrors = defaultShaders.passThroughWCubeMap->addProgram_srcCode(defaultFragShader_CubeMapWorld,GL_FRAGMENT_SHADER);	assert(noDefaultShaderErrors);
+	defaultShaders.passThroughWCubeMap->linkAndRun();
+	defaultShaders.passThroughWCubeMap->saveUniform("viewTransform",ParameterType::PT_MAT4,&viewTransform[0][0]);
 
 	init();
 }
@@ -177,6 +215,19 @@ void WidgetRenderer::mouseMoveEvent(QMouseEvent* e) {
 	if(!disableCamMovement && GetAsyncKeyState(CAM_CONTROL_ENABLECLICK)!=0) {
 		glm::vec2 newPos(e->x(),e->y());
 		myCam.updateMousePos(newPos);
+	}
+}
+void WidgetRenderer::wheelEvent(QWheelEvent* e) {
+	int delta = e->delta();
+	const int increment = 75;
+	float percent = .1;
+	for (int i = 0; i < delta; i++)
+	{
+		myCam.moveForward(percent / increment);
+	}
+	for (int i = 0; i > delta; i--)
+	{
+		myCam.moveBackward(percent / increment);
 	}
 }
 void WidgetRenderer::setDebugMenu(DebugMenuManager * menu) {
