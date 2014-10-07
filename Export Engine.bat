1737017d50e8adb550de49102e9460772cba34e0
Anthony Corbin <dragon1672@gmail.com>
2014-10-06 23:23:35 -0600

    Moving up folder

--- Export Engine.bat
+++
@@ -1,5 +0,0 @@
-robocopy "./Engine/Engine" "./MiddleWare/EngineCorbin/include" *.h /S /R:1 /W:1 /NDL /XJD /XD OutputFolder
-robocopy "./Example Project/Debug" "./MiddleWare/EngineCorbin/bin" *.dll /S /R:1 /W:1 /NDL /XJD /XD OutputFolder
-robocopy "./Example Project/Debug" "./MiddleWare/EngineCorbin/lib" *.lib /S /R:1 /W:1 /NDL /XJD /XD OutputFolder
-echo Have a nice day! 
-exit /b 1
