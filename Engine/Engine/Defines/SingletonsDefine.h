ea6ef386ef49b617b17a075b2b6913b6c2d4e1cb
Anthony Corbin <dragon1672@gmail.com>
2014-10-08 22:17:33 -0600

    Adding del to singletons

--- Engine/Engine/Defines/SingletonsDefine.h
+++ Engine/Engine/Defines/SingletonsDefine.h
@@ -2,9 +2,11 @@
 #define DEFINE_SINGLETON(class_name) \
 	private: static class_name * _instance; \
 	public: static class_name##& getInstance(); \
+	public: static void delInstance(); \
 	private:
 
 //place in a CPP file
 #define IMPLEMENT_SINGLETON(class_name) \
 	class_name * class_name##::_instance = nullptr; \
-	class_name##& class_name##::getInstance() { return *(_instance == nullptr ? _instance = new class_name : _instance); }
+	class_name##& class_name##::getInstance() { return *(_instance == nullptr ? _instance = new class_name : _instance); } \
+	void class_name##::delInstance() { if(_instance == nullptr) delete _instance; _instance = nullptr; }
