2841a4d44ecf88778959d56ca0bdf71093136758
Anthony Corbin <dragon1672@gmail.com>
2014-10-08 22:17:42 -0600

    Pushing out Mesh to CPP

---
+++ Engine/Engine/Renderer/Geo/Mesh.cpp
@@ -0,0 +1,17 @@
+#include "Mesh.h"
+
+void Mesh::updateMinAndMax()
+{
+	for (uint i = 0; i < verts.size(); i++) {
+		min.x = i==0 || verts[i].pos.x < min.x ? verts[i].pos.x : min.x;
+		min.x = i==0 || verts[i].pos.y < min.y ? verts[i].pos.y : min.y;
+		max.x = i==0 || verts[i].pos.x < max.x ? verts[i].pos.x : max.x;
+		max.x = i==0 || verts[i].pos.y < max.y ? verts[i].pos.y : max.y;
+	}
+}
+
+void Mesh::passToHardware()
+{
+	geo.init(sizeof(Vert),(const void*)&verts[0],verts.size(),&indicees[0],indicees.size(),3);
+	geo.addStreamedParameters(Vert().getAtribs(),Vert().getNumOfAtribs());
+}
--- Engine/Engine/Renderer/Geo/Mesh.h
+++ Engine/Engine/Renderer/Geo/Mesh.h
@@ -11,26 +11,16 @@ class Mesh {
 public:
 	Mesh() {}
 	Mesh(Neumont::ShapeData& NU) {
-		for (int i = 0; i < NU.numVerts; i++)   { verts.push_back(Vert(NU.verts[i])); }
-		for (int i = 0; i < NU.numIndices; i++) { indicees.push_back(NU.indices[i]);  }
+		for (uint i = 0; i < NU.numVerts; i++)   { verts.push_back(Vert(NU.verts[i])); }
+		for (uint i = 0; i < NU.numIndices; i++) { indicees.push_back(NU.indices[i]);  }
 	}
 	std::vector<Vert> verts;
 	std::vector<uint> indicees;
 	glm::vec3 min;
 	glm::vec3 max;
-	void updateMinAndMax() {
-		for (int i = 0; i < verts.size(); i++) {
-			min.x = i==0 || verts[i].pos.x < min.x ? verts[i].pos.x : min.x;
-			min.x = i==0 || verts[i].pos.y < min.y ? verts[i].pos.y : min.y;
-			max.x = i==0 || verts[i].pos.x < max.x ? verts[i].pos.x : max.x;
-			max.x = i==0 || verts[i].pos.y < max.y ? verts[i].pos.y : max.y;
-		}
-	}
+	void updateMinAndMax();
 
 
-	void passToHardware() {
-		geo.init(sizeof(Vert),&verts[0],verts.size(),&indicees[0],indicees.size(),3);
-		int 
-	}
+	void passToHardware();
 
 };
