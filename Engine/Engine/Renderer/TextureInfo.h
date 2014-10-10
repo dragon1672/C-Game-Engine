8606679f3873e1ea1f4b19d9e5778f5bbfc93cb3
Anthony Corbin <dragon1672@gmail.com>
2014-10-10 14:21:21 -0600

    Adding ImageData

--- Engine/Engine/Renderer/TextureInfo.h
+++ Engine/Engine/Renderer/TextureInfo.h
@@ -2,16 +2,32 @@
 
 #include <Engine/unsigned.h>
 #include <glm/glm.hpp>
+#include <Engine/Renderer/ParameterType.h>
 
 class TextureInfo {
 public:
 	uint bufferID;
 	uint slotID;
+	ParameterType type;
 };
-class ImageInfo : public TextureInfo {
-	const char * src;
+
+struct ImageData {
+	ubyte * data;
 	uint width;
 	uint height;
-	glm::vec2 offset;
-	glm::vec2 scale;
+	GLenum type;
+	GLenum type2;
+	ImageData() : data(nullptr),
+		width((uint)-1), height((uint)-1),
+		type((GLenum)GL_RGBA), type2((GLenum)-1) {}
+	ImageData(QImage& src) {
+		init(src);
+		type = (GLenum)GL_RGBA;
+		type2 = (GLenum)-1;
+	}
+	void init(QImage& src) {
+		data = src.bits();
+		width = src.width();
+		height = src.height();
+	}
 };
