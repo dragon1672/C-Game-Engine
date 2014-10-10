d3626c6692c7a0933a7dffcfd62b6f6d5312092d
Anthony Corbin <dragon1672@gmail.com>
2014-10-09 19:58:27 -0600

    Defaults for Vert values

--- Engine/Engine/Renderer/Geo/Vert.h
+++ Engine/Engine/Renderer/Geo/Vert.h
@@ -7,7 +7,18 @@
 
 class Vert : public AutoAtrib {
 public:
-	Vert(){}
+	glm::vec3 pos;
+	glm::vec4 col;
+	glm::vec3 norm;
+	glm::vec3 tan;
+	glm::vec2 uv;
+	Vert() : 
+		pos(0,0,0),
+		col(1,1,1,1),
+		norm(0,0,0),
+		tan(0,0,0),
+		uv(0,0)
+	{}
 	Vert(Neumont::Vertex v) {
 		pos =  v.position;
 		col =  v.color;
@@ -16,9 +27,4 @@ public:
 	}
 	int * getAtribs();
 	int getNumOfAtribs();
-	glm::vec3 pos;
-	glm::vec4 col;
-	glm::vec3 norm;
-	glm::vec3 tan;
-	glm::vec2 uv;
 };
