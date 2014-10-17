9f71fd345ff42fc70e5bf99a4c2d3586ed19d657
Anthony Corbin <dragon1672@gmail.com>
2014-10-16 23:10:46 -0600

    Using Lua objects in Matrix info

--- Engine/Engine/Tools/MatrixInfo.cpp
+++ Engine/Engine/Tools/MatrixInfo.cpp
@@ -7,13 +7,22 @@ void MatrixInfo::lateUpdate() {
 glm::mat4& MatrixInfo::getTransform()     { return getCompleteTransform();     } // save this on in the Shader
 
 glm::mat4&  MatrixInfo::getRotMat() {
-	return rotChanged ? rotationMat = glm::rotate(rotation.x,glm::vec3(1,0,0))
-		* glm::rotate(rotation.y,glm::vec3(0,1,0))
-		* glm::rotate(rotation.z,glm::vec3(0,0,1))
-		: rotationMat;
+	if(rotChanged) rotationMat = glm::rotate(rot.x,glm::vec3(1,0,0))
+		* glm::rotate(rot.y,glm::vec3(0,1,0))
+		* glm::rotate(rot.z,glm::vec3(0,0,1));
+	rotChanged = false;
+	return rotationMat;
 }
-glm::mat4&  MatrixInfo::getScaleMat() { return scaleChanged ? scaleMat = glm::scale(scale) : scaleMat; }
-glm::mat4&  MatrixInfo::getTranslationMat() { return posChanged ? translationMat = glm::translate(position) : translationMat; }
+glm::mat4&  MatrixInfo::getScaleMat() {
+	if(scaleChanged) scaleMat = glm::scale(scale);
+	scaleChanged = false;
+	return scaleMat;
+}
+glm::mat4&  MatrixInfo::getTranslationMat() {
+	if(posChanged) translationMat = glm::translate(pos);
+	posChanged = false;
+	return translationMat;
+}
 glm::mat4&  MatrixInfo::getCompleteTransform() {
 	return scaleChanged || posChanged || rotChanged ? transform = getTranslationMat() * getRotMat() * getScaleMat() : transform;
 }
--- Engine/Engine/Tools/MatrixInfo.h
+++ Engine/Engine/Tools/MatrixInfo.h
@@ -7,6 +7,14 @@
 #include <Engine/Entity/Component.h>
 #include <Engine/Tools/GlmWrap.h>
 
+
+#define GET_SET_TRACK(var_type,set_set_type, name,Uppercase_name)\
+	var_type name;                                          \
+	bool name##Changed;                                     \
+	inline var_type get##Uppercase_name##() { return name; } \
+	inline LuaUserdata<var_type> getLua##Uppercase_name##() { return name; } \
+	inline void set##Uppercase_name##(##set_set_type##& toSet) { name = toSet; name##Changed = true; }
+
 class ENGINE_SHARED MatrixInfo : public Component {
 	private:
 		glm::mat4 transform;
@@ -14,9 +22,9 @@ class ENGINE_SHARED MatrixInfo : public Component {
 		glm::mat4 translationMat;
 		glm::mat4 scaleMat;
 
-		GET_SET_TRACK_VEC3(pos,position);
-		GET_SET_TRACK_VEC3(rot,rotation);
-		GET_SET_TRACK_VEC3(scale,scale);
+		GET_SET_TRACK(wrap::vec3,glm::vec3,pos,Pos);
+		GET_SET_TRACK(wrap::vec3,glm::vec3,rot,Rot);
+		GET_SET_TRACK(wrap::vec3,glm::vec3,scale,Scale);
 
 	public:
 		MatrixInfo() : scale(1,1,1), posChanged(true), rotChanged(true), scaleChanged(true) {}
