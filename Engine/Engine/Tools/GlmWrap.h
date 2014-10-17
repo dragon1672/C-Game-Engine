ef1e8bb9123327f2ae3a72fbbdb3e2d9f3d9b3b3
Anthony Corbin <dragon1672@gmail.com>
2014-10-16 21:22:34 -0600

    Improved Binding

--- Engine/Engine/Tools/GlmWrap.h
+++ Engine/Engine/Tools/GlmWrap.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include <glm/glm.hpp>
-#include <Engine/Tools/BindFloat.h>
 #include <Engine/Tools/MasterLua.h>
 
-#define MAKE_GETTER_SETTER(Uppercase,LowerCase)\
-	float get##Uppercase##() { return LowerCase; }                 \
-	void  set##Uppercase##(float LowerCase) { this->LowerCase = LowerCase; }
+#define LUA_VECTOR_MAKE_GETTER_SETTER(Uppercase,LowerCase)\
+	inline float get##Uppercase##() { return LowerCase; }                 \
+	inline void  set##Uppercase##(float LowerCase) { this->LowerCase = LowerCase; }
 #define LUA_STRING(x) #x
 #define LUA_GLEW(a,b) LUA_STRING(a##b)
 #define LUA_VECTOR_BIND(vec_type,var_name,UppercaseLetter)\
@@ -15,34 +14,17 @@
 
 namespace wrap {
 
-	class vec2 {
-	private:
-		glm::vec2 back;
+	class vec2 : glm::vec2 {
 	public:
-		union {
-			struct {
-				BoundFloat x;
-				BoundFloat y;
-			};
-			struct {
-				BoundFloat r;
-				BoundFloat g;
-			};
-		};
-		vec2() :
-			x(back.x),	r(back.x),
-			y(back.y),	g(back.y)
-		{}
-		MAKE_GETTER_SETTER(X,x);
-		MAKE_GETTER_SETTER(Y,y);
+		LUA_VECTOR_MAKE_GETTER_SETTER(X,x);
+		LUA_VECTOR_MAKE_GETTER_SETTER(Y,y);
 
-		MAKE_GETTER_SETTER(R,r);
-		MAKE_GETTER_SETTER(G,g);
+		LUA_VECTOR_MAKE_GETTER_SETTER(R,r);
+		LUA_VECTOR_MAKE_GETTER_SETTER(G,g);
 		void  set(float x, float y) {
 			this->x = x;
 			this->y = y;
 		}
-		inline operator glm::vec2&() { return back; }
 
 		inline operator LuaUserdata<vec2>() {
 			MAKE_LUA_INSTANCE_RET(vec2,ret);
@@ -56,40 +38,20 @@ namespace wrap {
 		}
 	};
 
-	class vec3 {
-	private:
-		glm::vec3 back;
+	class vec3 : glm::vec3 {
 	public:
-		union {
-			struct {
-				BoundFloat x;
-				BoundFloat y;
-				BoundFloat z;
-			};
-			struct {
-				BoundFloat r;
-				BoundFloat g;
-				BoundFloat b;
-			};
-		};
-		vec3() :
-			x(back.x),	r(back.x),
-			y(back.y),	g(back.y),
-			z(back.z),	b(back.z)
-		{}
-		MAKE_GETTER_SETTER(X,x);
-		MAKE_GETTER_SETTER(Y,y);
-		MAKE_GETTER_SETTER(Z,z);
+		LUA_VECTOR_MAKE_GETTER_SETTER(X,x);
+		LUA_VECTOR_MAKE_GETTER_SETTER(Y,y);
+		LUA_VECTOR_MAKE_GETTER_SETTER(Z,z);
 
-		MAKE_GETTER_SETTER(R,r);
-		MAKE_GETTER_SETTER(G,g);
-		MAKE_GETTER_SETTER(B,b);
+		LUA_VECTOR_MAKE_GETTER_SETTER(R,r);
+		LUA_VECTOR_MAKE_GETTER_SETTER(G,g);
+		LUA_VECTOR_MAKE_GETTER_SETTER(B,b);
 		void  set(float x, float y, float z, float w) {
 			this->x = x;
 			this->y = y;
 			this->z = z;
 		}
-		inline operator glm::vec3&() { return back; }
 
 		inline operator LuaUserdata<vec3>() {
 			MAKE_LUA_INSTANCE_RET(vec3,ret);
@@ -105,46 +67,23 @@ namespace wrap {
 		}
 	};
 
-	class vec4 {
-	private:
-		glm::vec4 back;
+	class vec4 : public glm::vec4 {
 	public:
-		union {
-			struct {
-				BoundFloat x;
-				BoundFloat y;
-				BoundFloat z;
-				BoundFloat w;
-			};
-			struct {
-				BoundFloat r;
-				BoundFloat g;
-				BoundFloat b;
-				BoundFloat a;
-			};
-		};
-		vec4() :
-			x(back.x),	r(back.x),
-			y(back.y),	g(back.y),
-			z(back.z),	b(back.z),
-			w(back.w),	a(back.w)
-		{}
-		MAKE_GETTER_SETTER(X,x);
-		MAKE_GETTER_SETTER(Y,y);
-		MAKE_GETTER_SETTER(Z,z);
-		MAKE_GETTER_SETTER(W,w);
+		LUA_VECTOR_MAKE_GETTER_SETTER(X,x);
+		LUA_VECTOR_MAKE_GETTER_SETTER(Y,y);
+		LUA_VECTOR_MAKE_GETTER_SETTER(Z,z);
+		LUA_VECTOR_MAKE_GETTER_SETTER(W,w);
 
-		MAKE_GETTER_SETTER(R,r);
-		MAKE_GETTER_SETTER(G,g);
-		MAKE_GETTER_SETTER(B,b);
-		MAKE_GETTER_SETTER(A,a);
+		LUA_VECTOR_MAKE_GETTER_SETTER(R,r);
+		LUA_VECTOR_MAKE_GETTER_SETTER(G,g);
+		LUA_VECTOR_MAKE_GETTER_SETTER(B,b);
+		LUA_VECTOR_MAKE_GETTER_SETTER(A,a);
 		void  set(float x, float y, float z, float w) {
 			this->x = x;
 			this->y = y;
 			this->z = z;
 			this->w = w;
 		}
-		inline operator glm::vec4&() { return back; }
 
 		inline operator LuaUserdata<vec4>() {
 			MAKE_LUA_INSTANCE_RET(vec4,ret);
