bdb90f7510d7cbd23bd9dc56206e7847f6e398cc
Anthony Corbin <dragon1672@gmail.com>
2014-10-19 04:34:02 -0600

    Add Shader and Mesh to Resource Manager

--- Engine/Engine/Systems/ResourceManager.cpp
+++ Engine/Engine/Systems/ResourceManager.cpp
@@ -1,3 +1,87 @@
 #include "ResourceManager.h"
 
 IMPLEMENT_SINGLETON(ResourceManager);
+
+Mesh * ResourceManager::addMesh()
+{
+	geos.push_back(Mesh());
+	MeshObjs.Register(geos.last());
+	return &geos.last();
+}
+
+Mesh * ResourceManager::addMesh()
+{
+
+}
+
+Mesh * ResourceManager::addMesh(Neumont::ShapeData& NUCrap)
+{
+	geos.push_back(Mesh());
+	Mesh& ret = geos.last();
+	for (int i = 0; i < NUCrap.numVerts; i++) {
+		auto& v = NUCrap.verts[i];
+		ret.verts.push_back(Vert(v.position,v.color,v.position,glm::vec4(),v.uv));
+	}
+	for (int i = 0; i < NUCrap.numIndices; i++) {
+		ret.indices.push_back(NUCrap.indices[i]);
+	}
+	MeshObjs.Register(geos.last());
+	return &geos.last();
+}
+
+Mesh * ResourceManager::addMesh(Mesh * toCopy)
+{
+	geos.push_back(Mesh(*toCopy));
+	MeshObjs.Register(geos.last());
+	return &geos.last();
+}
+
+Mesh * ResourceManager::addMesh(std::string filePath)
+{
+	return addMesh(filePath.c_str());
+}
+
+Mesh * ResourceManager::addMesh(const char * filePath)
+{
+	geos.push_back(FileIO::loadMeshFromFile(filePath));
+	MeshObjs.Register(geos.last());
+	return &geos.last();
+}
+
+ShaderProgram * ResourceManager::addShader_file(const char * vertFilePath, const char * fragFilePath)
+{
+	return addShader_src(FileIO::readFile(vertFilePath),FileIO::readFile(fragFilePath));
+}
+
+ShaderProgram * ResourceManager::addShader_file(std::string vertFilePath, std::string fragFilePath)
+{
+	return addShader_src(FileIO::readFile(vertFilePath),FileIO::readFile(fragFilePath));
+}
+
+ShaderProgram * ResourceManager::addShader_src(std::string vert, std::string frag)
+{
+	return addShader_src(vert.c_str(),frag.c_str());
+}
+
+ShaderProgram * ResourceManager::addShader_src(const char * vert, const char * frag)
+{
+	shaders.push_back(ShaderProgram());
+	ShaderObjs.Register(geos.last());
+	shaders.last().buildBasicProgram(vert,frag);
+	return &shaders.last();
+}
+
+void ResourceManager::init()
+{
+	foreachOnAll([](Resource& data) { data.init(); });
+}
+
+void ResourceManager::PassDownToHardWare()
+{
+	foreachOnAll([](Resource& data) { data.PassDownToHardWare(); });
+}
+
+void ResourceManager::update()
+{
+	foreachOnAll([](Resource& data) { data.update(); });
+}
--- Engine/Engine/Systems/ResourceManager.h
+++ Engine/Engine/Systems/ResourceManager.h
@@ -5,17 +5,45 @@
 #include <Engine/Systems/Resources/TextureInfo.h>
 #include <Engine/Systems/Resources/Mesh.h>
 #include <Engine/Systems/Resources/ShaderProgram.h>
+#include <Engine/Systems/ObjectManager.h>
+#include <ShapeData.h>
+#include <functional>
+#include <Engine/IO/FileIO.h>
+#include <Engine/IO/MeshLoader.h>
 
 #define resourceManager
 
-class ResourceManager {
+class ResourceManager : public Resource {
 	DEFINE_SINGLETON(ResourceManager);
 private:
 	ConstVector<ShaderProgram> shaders;
-	ConstVector<Mesh> geo;
+	ObjectManager ShaderObjs;
+	ConstVector<Mesh> geos;
+	ObjectManager MeshObjs;
 	ConstVector<TextureInfo> textures;
-	//ConstVector<
-
+	ObjectManager TextureObjs;
+	void foreachOnAll(std::function<void(Resource&)> func) {
+		for (int i = 0; i < shaders.size();  i++) func(shaders[i]);
+		for (int i = 0; i < geos.size();     i++) func(geos[i]);
+		for (int i = 0; i < textures.size(); i++) func(textures[i]);
+	}
 public:
-	
+
+	Mesh * addMesh();
+	Mesh * addMesh(Neumont::ShapeData& NUCrap);
+	Mesh * addMesh(Mesh * toCopy);
+	Mesh * addMesh(std::string filePath);
+	Mesh * addMesh(const char * filePath);
+
+	ShaderProgram * addShader_file(const char * vertFilePath, const char * fragFilePath);
+	ShaderProgram * addShader_file(std::string vertFilePath, std::string fragFilePath);
+	ShaderProgram * addShader_src(std::string vert, std::string frag);
+	ShaderProgram * addShader_src(const char * vert, const char * frag);
+
+
+	void init();
+
+	void PassDownToHardWare();
+
+	void update();
 };
