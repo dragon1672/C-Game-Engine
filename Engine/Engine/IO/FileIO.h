6891b72f1a490f9eb16103ed47ac7181b3655db8
Anthony Corbin <dragon1672@gmail.com>
2014-10-19 05:35:01 -0600

    Expanding loop unwind

--- Engine/Engine/IO/FileIO.h
+++ Engine/Engine/IO/FileIO.h
@@ -20,17 +20,40 @@ namespace FileIO {
 
 	template<typename T>
 	void myMemCopy(T * from, T * to, uint size) {
-		int n = (size + 7) / 8;
-		switch(size % 8) {
+		int n = (size + 30) / 31;
+		switch(size % 31) {
 		case 0:
-			do {	*to++ = *from++;
-		case 7:		*to++ = *from++;
-		case 6:		*to++ = *from++;
-		case 5:		*to++ = *from++;
-		case 4:		*to++ = *from++;
-		case 3:		*to++ = *from++;
-		case 2:		*to++ = *from++;
-		case 1:		*to++ = *from++;
+			do {    	*to++ = *from++;
+		case 30:		*to++ = *from++;
+		case 29:		*to++ = *from++;
+		case 28:		*to++ = *from++;
+		case 27:		*to++ = *from++;
+		case 26:		*to++ = *from++;
+		case 25:		*to++ = *from++;
+		case 24:		*to++ = *from++;
+		case 23:		*to++ = *from++;
+		case 22:		*to++ = *from++;
+		case 21:		*to++ = *from++;
+		case 20:		*to++ = *from++;
+		case 19:		*to++ = *from++;
+		case 18:		*to++ = *from++;
+		case 17:		*to++ = *from++;
+		case 16:		*to++ = *from++;
+		case 15:		*to++ = *from++;
+		case 14:		*to++ = *from++;
+		case 13:		*to++ = *from++;
+		case 12:		*to++ = *from++;
+		case 11:		*to++ = *from++;
+		case 10:		*to++ = *from++;
+		case 9: 		*to++ = *from++;
+		case 8: 		*to++ = *from++;
+		case 7: 		*to++ = *from++;
+		case 6: 		*to++ = *from++;
+		case 5: 		*to++ = *from++;
+		case 4: 		*to++ = *from++;
+		case 3: 		*to++ = *from++;
+		case 2: 		*to++ = *from++;
+		case 1: 		*to++ = *from++;
 			} while(--n > 0);
 		}
 	}
