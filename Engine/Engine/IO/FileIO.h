9896fcc6b3cc8f52daa2b3f7defff99fe8616aa8
Anthony Corbin <dragon1672@gmail.com>
2014-10-19 05:24:59 -0600

    Adding myMemCopy to FileIO

--- Engine/Engine/IO/FileIO.h
+++ Engine/Engine/IO/FileIO.h
@@ -17,4 +17,21 @@ namespace FileIO {
 	FileData loadFile(const char * filePath);
 	bool validFile(std::string filePath);
 	bool validFile(const char * filePath);
+
+	template<typename T>
+	void myMemCopy(T * from, T * to, uint size) {
+		int n = (size + 7) / 8;
+		switch(size % 8) {
+		case 0:
+			do {	*to++ = *from++;
+		case 7:		*to++ = *from++;
+		case 6:		*to++ = *from++;
+		case 5:		*to++ = *from++;
+		case 4:		*to++ = *from++;
+		case 3:		*to++ = *from++;
+		case 2:		*to++ = *from++;
+		case 1:		*to++ = *from++;
+			} while(--n > 0);
+		}
+	}
 }
