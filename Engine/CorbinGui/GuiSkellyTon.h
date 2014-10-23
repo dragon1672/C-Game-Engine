0ca76ec846b6d0f4664b56442696dde953ab6ed5
Anthony Corbin <dragon1672@gmail.com>
2014-10-22 22:34:13 -0600

    Tree Widget demo

--- Engine/CorbinGui/GuiSkellyTon.h
+++ Engine/CorbinGui/GuiSkellyTon.h
@@ -5,6 +5,7 @@
 #include <QtWidgets/QMenu.h>
 #include <QtWidgets/QMenuBar>
 #include <ExportHeader.h>
+#include <QtWidgets/QTreeWidget>
 
 #include <Engine/Tools/Printer.h>
 #include <CorbinGui/BasicQGLGui.h>
@@ -13,13 +14,15 @@ class ENGINE_SHARED GuiSkellyTon : public QMainWindow  {
 	QMenu * fileMenu;
 	QTimer myTimer;
 	BasicQGLGui * scene;
+	QVBoxLayout * layout;
 public:
 	GuiSkellyTon()
 		: scene(new BasicQGLGui())
 	{
+		scene->setMinimumSize(500,500);
 		connect(&myTimer,&QTimer::timeout,[this](){ this->update(); });
 
-		QVBoxLayout *layout = new QVBoxLayout;
+		layout = new QVBoxLayout;
 		//setup mainwidget
 		QWidget * window = new QWidget();
 		window->setLayout(layout);
@@ -54,6 +57,24 @@ public:
 		fileMenu->addAction(action = new QAction("Renderable Component", this));	connect(action, &QAction::triggered, [this](){ printer.LogMessage("Add Renderable Clicked"); });
 		fileMenu->addAction(action = new QAction("collider Component", this));	connect(action, &QAction::triggered, [this](){ printer.LogMessage("Add collider Clicked"); });
 		fileMenu->addAction(action = new QAction("Script Component", this));	connect(action, &QAction::triggered, [this](){ printer.LogMessage("Add Script Clicked"); });
+
+
+
+		QTreeWidget *treeWidget = new QTreeWidget();
+		treeWidget->setColumnCount(1);
+		QList<QTreeWidgetItem *> items;
+		for (int i = 0; i < 5; ++i) {
+			QTreeWidgetItem *treeWidget2 = new QTreeWidgetItem((QTreeWidget*)0,QStringList(QString("item: %1").arg(i)));
+			QList<QTreeWidgetItem *> items2;
+			for (int i = 0; i < 5; ++i) {
+				items2.append(new QTreeWidgetItem((QTreeWidget*)0, QStringList(QString("item: %1").arg(i))));
+			}
+			treeWidget2->addChildren(items2);
+			items.append(treeWidget2);
+		}
+		treeWidget->insertTopLevelItems(0, items);
+
+		layout->addWidget(treeWidget);
 	}
 
 	Entity * addEntity(const char * name) {
