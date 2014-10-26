e3c8114b2117570cab044a339703968f6adee564
Anthony Corbin <dragon1672@gmail.com>
2014-10-25 18:10:56 -0600

    Collection edits for unsigned warnings and copy errors

--- Engine/Engine/Tools/CollectionEditing.h
+++ Engine/Engine/Tools/CollectionEditing.h
@@ -16,7 +16,7 @@ namespace Collections {
 
 	//vector
 	template <typename T> int	 find(std::vector<T>& array, T toFind) {
-		for (int i = 0; i < array.size(); i++)
+		for (uint i = 0; i < array.size(); i++)
 			if(array[i] == toFind) return i;
 		return -1
 	}
@@ -30,7 +30,7 @@ namespace Collections {
 	}
 	template <typename T> T SingleSelect(std::vector<T>& array, std::function<T(T&,T&)> selector) {
 		T ret = T();
-		for(unsigned int i=0;i<array.size();i++) {
+		for(uint i=0;i<array.size();i++) {
 			ret = i==0 ? array[i] : selector(array[i],ret);
 		};
 		return ret;
@@ -51,16 +51,16 @@ namespace Collections {
 	inline long   Sum(std::vector<long   > array) { return SingleSelect<long  >(array,[](long   a, long   b) -> long   { return a + b; }); }
 	inline float  Sum(std::vector<float  > array) { return SingleSelect<float >(array,[](float  a, float  b) -> float  { return a + b; }); }
 	inline double Sum(std::vector<double > array) { return SingleSelect<double>(array,[](double a, double b) -> double { return a + b; }); }
-	template <typename T, typename R> std::vector<R> Select(std::vector<T> array, std::function<R(T&)> selector) {
+	template <typename T, typename R> std::vector<R> Select(std::vector<T>& array, std::function<R(T&)> selector) {
 		std::vector<R> ret;
-		for(int i=0;i<array.size();i++) ret.push_back(selector(array[i]));
+		for(uint i=0;i<array.size();i++) ret.push_back(selector(array[i]));
 		return ret;
 	}
 
 
 	//const vec
 	template <typename T> int	 find(ConstVector<T>& array, T toFind) {
-		for (int i = 0; i < array.size(); i++)
+		for (uint i = 0; i < array.size(); i++)
 			if(array[i] == toFind) return i;
 		return -1
 	}
@@ -69,12 +69,12 @@ namespace Collections {
 	}
 	template <typename T> std::vector<T> Where(ConstVector<T>& array, std::function<bool(T&)> condition) {
 		std::vector<T> ret;
-		for(int i=0;i<array.size();i++) if(condition(array[i])) ret.push_back(array[i]);
+		for(uint i=0;i<array.size();i++) if(condition(array[i])) ret.push_back(array[i]);
 		return ret;
 	}
 	template <typename T> T SingleSelect(ConstVector<T>& array, std::function<T(T&,T&)> selector) {
 		T ret = T();
-		for(unsigned int i=0;i<array.size();i++) {
+		for(uint i=0;i<array.size();i++) {
 			ret = i==0 ? array[i] : selector(array[i],ret);
 		};
 		return ret;
@@ -95,7 +95,7 @@ namespace Collections {
 	inline long   Sum(ConstVector<long   > array) { return SingleSelect<long  >(array,[](long   a, long   b) -> long   { return a + b; }); }
 	inline float  Sum(ConstVector<float  > array) { return SingleSelect<float >(array,[](float  a, float  b) -> float  { return a + b; }); }
 	inline double Sum(ConstVector<double > array) { return SingleSelect<double>(array,[](double a, double b) -> double { return a + b; }); }
-	template <typename T, typename R> std::vector<R> Select(ConstVector<T> array, std::function<R(T&)> selector) {
+	template <typename T, typename R> std::vector<R> Select(ConstVector<T>& array, std::function<R(T&)> selector) {
 		std::vector<R> ret;
 		for(uint i=0;i<array.size();i++) ret.push_back(selector(array[i]));
 		return ret;
@@ -149,8 +149,8 @@ namespace Collections {
 	}
 
 
-	template<typename T, typename R, typename X> std::vector<T> RICVec(std::vector<R,X> vec) {
-		std::vector<T> ret(vec.size());
+	template<typename T, typename R, typename X> std::vector<T> RICVec(std::vector<R,X>& vec) {
+		std::vector<T> ret;
 		for (uint i = 0; i < vec.size(); i++) ret.push_back((T)vec[i]);
 		return ret;
 	}
