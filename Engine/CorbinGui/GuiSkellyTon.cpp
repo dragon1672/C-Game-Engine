8bbb24077116acade64a59fbe74bedcb732e032b
Anthony Corbin <dragon1672@gmail.com>
2014-10-29 15:19:22 -0600

    Formatting

--- Engine/CorbinGui/GuiSkellyTon.cpp
+++ Engine/CorbinGui/GuiSkellyTon.cpp
@@ -75,15 +75,29 @@ void GuiSkellyTon::initBar()
 		printer.LogMessage("Save Clicked");
 	});
 
-	fileMenu->addAction(action = new QAction("Exit Program", this));	connect(action, &QAction::triggered, [this](){ printer.LogMessage("Exit Clicked"); this->close(); });
+	fileMenu->addAction(action = new QAction("Exit Program", this));	connect(action, &QAction::triggered, [this](){
+		printer.LogMessage("Exit Clicked"); this->close();
+	});
 
 	fileMenu = menuBar()->addMenu("Resources");
-	fileMenu->addAction(action = new QAction("Load Obj", this));		connect(action, &QAction::triggered, [this](){ printer.LogMessage("Load Obj Clicked"); });
-	fileMenu->addAction(action = new QAction("Load Texture", this));	connect(action, &QAction::triggered, [this](){ printer.LogMessage("Load Texture Clicked"); });
-	fileMenu->addAction(action = new QAction("Load Shader", this));		connect(action, &QAction::triggered, [this](){ printer.LogMessage("Load Shader Clicked"); });
-	fileMenu->addAction(action = new QAction("Load Script", this));		connect(action, &QAction::triggered, [this](){ printer.LogMessage("Load Script Clicked"); });
+	fileMenu->addAction(action = new QAction("Load Obj", this));        connect(action, &QAction::triggered, [this](){
+		printer.LogMessage("Load Obj Clicked");
+	});
+	fileMenu->addAction(action = new QAction("Load Texture", this));    connect(action, &QAction::triggered, [this](){
+		printer.LogMessage("Load Texture Clicked");
+	});
+	fileMenu->addAction(action = new QAction("Load Shader", this));     connect(action, &QAction::triggered, [this](){
+		printer.LogMessage("Load Shader Clicked");
+	});
+	fileMenu->addAction(action = new QAction("Load Script", this));     connect(action, &QAction::triggered, [this](){
+		printer.LogMessage("Load Script Clicked");
+	});
 	fileMenu = menuBar()->addMenu("GameObject");
-	fileMenu->addAction(action = new QAction("New Game Object", this));	connect(action, &QAction::triggered, [this](){ game->AddEntity("New Game Object"); });
+	fileMenu->addAction(action = new QAction("New Game Object", this));	connect(action, &QAction::triggered, [this](){
+		game->AddEntity("New Game Object");
+	});
+
+
 	addComponentBar = fileMenu->addMenu("Add Component");
 	addComponentBar->addAction(action = new QAction("Renderable Component", this));	connect(action, &QAction::triggered, [this](){
 		this->game->currentEntity.addComponent<RenderableComponent>();
