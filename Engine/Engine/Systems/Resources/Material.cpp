faf3416045848b5bbb7542927e85efb55f1a26a9
Anthony Corbin <dragon1672@gmail.com>
2014-11-12 14:47:21 -0700

    Material code cleanup, Material now streamable

--- Engine/Engine/Systems/Resources/Material.cpp
+++ Engine/Engine/Systems/Resources/Material.cpp
@@ -1,6 +1,34 @@
 #include "Material.h"
+#include <Engine/IO/Stream.h>
 
 
+#pragma region define helpers
+#define MATERIAL_CONSTR_TEXTURE(name,properCase)\
+	name = nullptr; \
+	uniformIndex##properCase = -1; \
+	has##properCase = false; \
+	name##Offset.x = name##Offset.y = 0; \
+	name##Scale.x  = name##Scale.y  = 1; \
+
+#define MATERIAL_STRING(a) #a
+#define MATERIAL_STRINGY(a,b) MATERIAL_STRING(a##b)
+
+#define MATERIAL_ADD_UNIFORM(arrayName,indexName,name,properCase)\
+	uniformIndex##properCase = indexName; \
+	arrayName##[##indexName##++] = ShaderUniformPram( MATERIAL_STRING(name)            , ParameterType::PT_TEXTURE2D, name             );\
+	arrayName##[##indexName##++] = ShaderUniformPram( MATERIAL_STRINGY(has,properCase) , ParameterType::PT_BOOLEAN,   &has##properCase );\
+	arrayName##[##indexName##++] = ShaderUniformPram( MATERIAL_STRINGY(name,Scale)     , ParameterType::PT_VEC2,      &name##Scale[0]  );\
+	arrayName##[##indexName##++] = ShaderUniformPram( MATERIAL_STRINGY(name,Offset)    , ParameterType::PT_VEC2,      &name##Offset[0] )
+
+#define SAVE_TEXTURE_TO_STREAM(stream_name,name,Propercase)\
+	stream_name << name->getID() << has##Propercase << name##Scale << name##Offset
+
+#define LOAD_TEXTURE_FROM_STREAM(stream_name,name,Propercase)\
+	int temp##Propercase##Id;                                                              \
+	stream_name >> temp##Propercase##Id >> has##Propercase >> name##Scale >> name##Offset; \
+	Propercase##(temp##Propercase##Id)
+
+#pragma  endregion
 
 ShaderUniformPram * Material::getUniforms()
 {
@@ -13,4 +41,38 @@ int Material::numOfUniforms()
 std::string Material::getShaderName()
 {
 	return "Material";
+}
+
+void Material::Save(Stream&s)
+{
+	
+	SAVE_TEXTURE_TO_STREAM(s,diffuse,  Diffuse  );
+	SAVE_TEXTURE_TO_STREAM(s,normalMap,NormalMap);
+	SAVE_TEXTURE_TO_STREAM(s,ambOcc,   AmbOcc   );
+	SAVE_TEXTURE_TO_STREAM(s,alphaMask,AlphaMask);
+}
+
+
+
+void Material::Load(Stream&s)
+{
+	LOAD_TEXTURE_FROM_STREAM(s,diffuse,  Diffuse  );
+	LOAD_TEXTURE_FROM_STREAM(s,normalMap,NormalMap);
+	LOAD_TEXTURE_FROM_STREAM(s,ambOcc,   AmbOcc   );
+	LOAD_TEXTURE_FROM_STREAM(s,alphaMask,AlphaMask);
+}
+
+Material::Material() :
+	diffuse(nullptr), normalMap(nullptr), ambOcc(nullptr), alphaMask(nullptr)
+{
+	MATERIAL_CONSTR_TEXTURE(diffuse,  Diffuse  );
+	MATERIAL_CONSTR_TEXTURE(normalMap,NormalMap);
+	MATERIAL_CONSTR_TEXTURE(ambOcc,   AmbOcc   );
+	MATERIAL_CONSTR_TEXTURE(alphaMask,AlphaMask);
+
+	int index = 0;
+	MATERIAL_ADD_UNIFORM(uniforms,index,diffuse,  Diffuse  );
+	MATERIAL_ADD_UNIFORM(uniforms,index,normalMap,NormalMap);
+	MATERIAL_ADD_UNIFORM(uniforms,index,ambOcc,   AmbOcc   );
+	MATERIAL_ADD_UNIFORM(uniforms,index,alphaMask,AlphaMask);
 }
--- Engine/Engine/Systems/Resources/Material.h
+++ Engine/Engine/Systems/Resources/Material.h
@@ -6,7 +6,9 @@
 #include <Engine/Tools/GlmWrap.h>
 #include <vector>
 #include <Engine/Systems/ResourceManager.h>
+#include <Engine/IO/StreamableObject.h>
 
+#pragma region define helpers
 #define MATERIAL_TEXTURE_GET_SET_WRAP(name,properCase) \
 	inline TextureInfo * properCase##() const { return name##; }      \
 	inline void properCase##(TextureInfo * val) {                     \
@@ -14,7 +16,7 @@
 		has##properCase = name != nullptr;                            \
 		uniforms[##uniformIndex##properCase##].Pointer(name);         \
 	}                                                                 \
-	inline void properCase##(int index) {                             \
+	inline void properCase##(double index) {                          \
 		name = resourceManager.getTextureInfo(##index##);             \
 		has##properCase = name != nullptr;                            \
 		uniforms[##uniformIndex##properCase##].Pointer(name);         \
@@ -22,28 +24,12 @@
 	inline glm::vec2& properCase##Scale()  { return name##Scale; }    \
 	inline glm::vec2& properCase##Offset() { return name##Offset; }
 
-
-
 #define MATERIAL_INIT_TEXTURE(name,properCase)\
 	TextureInfo * name; bool has##properCase; glm::vec2 name##Scale, name##Offset; int uniformIndex##properCase;
-#define MATERIAL_CONSTR_TEXTURE(name,properCase)\
-	name = nullptr; \
-	uniformIndex##properCase = -1; \
-	has##properCase = false; \
-	name##Offset.x = name##Offset.y = 0; \
-	name##Scale.x  = name##Scale.y  = 1; \
 
-#define MATERIAL_STRING(a) #a
-#define MATERIAL_STRINGY(a,b) MATERIAL_STRING(a##b)
+#pragma  endregion
 
-#define MATERIAL_ADD_UNIFORM(arrayName,indexName,name,properCase)\
-	uniformIndex##properCase = indexName; \
-	arrayName##[##indexName##++] = ShaderUniformPram( MATERIAL_STRING(name)            , ParameterType::PT_TEXTURE2D, name             );\
-	arrayName##[##indexName##++] = ShaderUniformPram( MATERIAL_STRINGY(has,properCase) , ParameterType::PT_BOOLEAN,   &has##properCase );\
-	arrayName##[##indexName##++] = ShaderUniformPram( MATERIAL_STRINGY(name,Scale)     , ParameterType::PT_VEC2,      &name##Scale[0]  );\
-	arrayName##[##indexName##++] = ShaderUniformPram( MATERIAL_STRINGY(name,Offset)    , ParameterType::PT_VEC2,      &name##Offset[0] )
-
-class ENGINE_SHARED Material : public ShaderObject {
+class ENGINE_SHARED Material : public ShaderObject, public StreamableObject {
 	ShaderUniformPram uniforms[16];
 	MATERIAL_INIT_TEXTURE(diffuse,  Diffuse  );
 	MATERIAL_INIT_TEXTURE(normalMap,NormalMap);
@@ -55,21 +41,7 @@ public:
 	MATERIAL_TEXTURE_GET_SET_WRAP(ambOcc,   AmbOcc   );
 	MATERIAL_TEXTURE_GET_SET_WRAP(alphaMask,AlphaMask);
 
-	Material()
-		:
-		diffuse(nullptr), normalMap(nullptr), ambOcc(nullptr), alphaMask(nullptr)
-	{
-		MATERIAL_CONSTR_TEXTURE(diffuse,  Diffuse  );
-		MATERIAL_CONSTR_TEXTURE(normalMap,NormalMap);
-		MATERIAL_CONSTR_TEXTURE(ambOcc,   AmbOcc   );
-		MATERIAL_CONSTR_TEXTURE(alphaMask,AlphaMask);
-
-		int index = 0;
-		MATERIAL_ADD_UNIFORM(uniforms,index,diffuse,  Diffuse  );
-		MATERIAL_ADD_UNIFORM(uniforms,index,normalMap,NormalMap);
-		MATERIAL_ADD_UNIFORM(uniforms,index,ambOcc,   AmbOcc   );
-		MATERIAL_ADD_UNIFORM(uniforms,index,alphaMask,AlphaMask);
-	}
+	Material();
 	wrap::vec4 color;
 
 	virtual ShaderUniformPram * getUniforms();
@@ -77,5 +49,9 @@ public:
 	virtual int numOfUniforms();
 
 	virtual std::string getShaderName();
+
+	virtual void Save(Stream&s);
+
+	virtual void Load(Stream&s);
 
 };
